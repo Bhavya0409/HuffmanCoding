72dab823490495f3804b37dd156c7bf40296def3
Bhavya Shah <bhavyashah@Bhavyas-MBP.home>
2018-10-01 15:47:41 -0400

    sort file by number of characters in input.dat

--- main.js
+++ main.js
@@ -10,7 +10,21 @@ getData = () => {
 }
 
 getData().then((data) => {
-  console.log('got file!', data);
+  const freq = {}
+  for (let i = 0; i < data.length; i++) {
+    const char = data.charAt(i);
+    if (freq.hasOwnProperty(char)) {
+      freq[char] = freq[char] + 1
+    } else {
+      freq[char] = 1
+    }
+  }
+
+  const freqSorted = Object.keys(freq).sort((a,b) => {
+    return freq[b]-freq[a];
+  })
+  console.log(freq);
+  console.log('freqSorted', freqSorted);
 }, (error) => {
   console.log('Error getting file');
 })
